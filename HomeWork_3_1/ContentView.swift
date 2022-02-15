8b3de4f6a7a771388dd1ef344f9e21806d4ec2ee
AliaksandrRauko <ravkoaal@mail.ru>
2022-02-15 14:44:40 +0300

    Initial Commit

---
+++ HomeWork_3_1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HomeWork_3_1
+//
+//  Created by Aliaksandr Rauko on 15.02.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HomeWork_3_1/HomeWork_3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork_3_1App.swift
+//  HomeWork_3_1
+//
+//  Created by Aliaksandr Rauko on 15.02.22.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork_3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
