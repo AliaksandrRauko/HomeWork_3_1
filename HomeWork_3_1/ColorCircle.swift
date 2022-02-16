99573ce53360838cba56fbb791c50968fbb54850
AliaksandrRauko <ravkoaal@mail.ru>
2022-02-16 09:16:44 +0300

    setup colorCircle

---
+++ HomeWork_3_1/ColorCircle.swift
@@ -0,0 +1,32 @@
+//
+//  ColorCircle.swift
+//  HomeWork_3_1
+//
+//  Created by Aliaksandr Rauko on 15.02.22.
+//
+
+import SwiftUI
+
+struct ColorCircle: View {
+    
+    let color: Color
+    
+    var body: some View {
+        Circle()
+
+            .foregroundColor(color)
+            .frame(width: 150, height: 150)
+            .overlay(Circle().stroke(Color.black,
+                                    lineWidth: 3))
+            .shadow(radius: 10)
+            .symbolRenderingMode(.hierarchical)
+            .foregroundColor(.red)
+            
+    }
+}
+
+struct ColorCircle_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorCircle(color: .red)
+    }
+}
--- HomeWork_3_1/ContentView.swift
+++ HomeWork_3_1/ContentView.swift
@@ -7,12 +7,54 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+enum CurrentLight {
+    case red, yellow, green
 }
+
+
+struct ContentView: View {
+    
+    @State private var currentLight = CurrentLight.red
+    let colors = [Color.red, Color.green, Color.green]
+    
+    var body: some View {
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [.purple, .white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                ColorCircle(color: .red)
+                    .padding()
+                ColorCircle(color: .yellow)
+                    .padding()
+                ColorCircle(color: .green)
+                    .padding()
+                Spacer()
+                
+                Button(action: {
+                    switch currentLight {
+                    case .red:
+                        currentLight = .yellow
+                        
+                    case .yellow:
+                        currentLight = .green
+                    case .green:
+                        currentLight = .red
+                    }
+                }) {
+                    Text("START")
+                        .font(.title)
+                        .controlSize(.large)
+                        .foregroundColor(.white)
+                        .background(.blue)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                }
+            }
+            .padding()
+        }
+    }
+    
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
